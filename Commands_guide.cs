2ab8c708b68b4539eb5ad0987ff7ee386d286421
Napitok01 <lisovskiylisovskiy228@gmail.com>
2022-08-06 14:17:19 +0300

    ColShapes, Markers Checkpoints and Blips

--- Commands_guide.cs
+++ Commands_guide.cs
@@ -96,4 +96,87 @@ namespace ServerSide : Script
     
 }
     
+
+
+
+    //! Про маркеры и чекпоинты :
+    /*
+        * Маркеры - это просто доп. элементы которые не несут ничего, а служат просто для
+        * того чтобы указать игроку что в этом месте что-то происходит обычно
+        * они работают с колшейпами
+        * чекпоинты - это как маркеры, ток в гонках GTA5 и можно указать направление
+    */
+    //! Создание маркеров :
+    [Command("marker")] //? комманда для создания красного маркера на позиции игрока : /marker (id маркера)
+
+        public void Marker(Player player, uint markerType) 
+        {
+            NAPI.Marker.CreateMarker(markerType, player.Position, new Vector3(), new Vector3(), 1f, new Color(255, 0, 0, 100), false, player.Dimension); //! 100 -- это прозрачность
+
+        }
+
+    //! Создание чекпоинтов :
+    [Command("checkpoint")] //? Команда для создания красного чекпоинта /checkpoint (id чекпоинта)
+
+        public void Checkpoint(Player player, uint checkpointType) //? в этом примере предусмотрено запоминание предыдущего чекпоинта из за этого каждый чекпоинт будет указывать на предыдущий
+        {
+            var direction = _prevCheckpoint?.Position ?? player.Position; 
+            _prevCheckpoint = NAPI.Checkpoint.CreateCheckpoint(checkpointType, player.Position + new Vector3(0f, 0f, -1f), direction, 1f, new Color(255, 0, 0, 100), player.Dimension);
+        }
+    
      
+    //! Теперь про Блипы :
+    /*
+        * Блипы это все точки на карте(и миникарте)
+        * они могут работать так : 1)видны всегда; 2) видны только когда игрок рядом; 
+    */
+
+    //! Создание блипов :
+    [Command("blip")] //? создание блипа на координатах игрока // sprite - id блипа, byte - непрозрачность(0-255)
+
+    public void Blip(Player player, uint sprite, byte color, string name, bool shortRange) //? /blip 1 1 Blip1 true
+    {
+        NAPI.Blip.CreateBlip(sprite, player.Position, 1f, color,  name, 255, 0f, shortRange, 0, player.Dimension)
+    }
+
+
+    //! Теперь про колшейпы
+    /*
+        * это зона которая определяет вошел или вышел ли кто-то из неё
+    */
+
+    //! Создание колшейпов:
+    [Command("colshape")] //? Создание колшейпа на позиции игрока с размером "scale" /colshape (радиус)
+    public void Colshape(Player player, float scale)
+    {
+        var colShape = NAPI.ColShape.CreateCylinderColShape(player.Position + new Vector3(0f, 0f, -1f), scale, 2f, player.Dimension);
+        //* colShape принимает 2 занчения OnEntityEnterColShape(вОшел) и OnEntityExitColShape(вЫшел) 
+        colShape.OnEntityEnterColShape += OnEntityEnterColShape;
+        colShape.OnEntityExitColShape += OnEntityExitColShape
+    }
+
+    private void OnEntityEnterColShape(ColShape colShape, Player player)
+    {
+        player.Armor = 100;
+    }
+
+    private void OnEntityExitColShape(ColShape colShape, Player player)
+    {
+        player.Armor = 0;
+    }
+
+
+    //! чтобы сделать колшейп видимым можно написать так:
+    /*
+    !заменить часть public void Colshape на : 
+    
+        * public void Colshape(Player player, float scale)
+        * {
+        *     var position = player.Position + new Vector3(0f, 0f, -1f);
+        * 
+        *     var colShape = NAPI.ColShape.CreateCylinderColShape(position, scale, 2f, player.Dimension);
+        *     colShape.SetData(nameof(GTANetworkAPI.Marker), NAPI.Marker.CreateMarker(1, position, new Vector3(), new Vector3(), new Color(255, 0, 0, 100), false, player.Dimension)); 
+        *     colShape.OnEntityEnterColShape += OnEntityEnterColShape;
+        *     colShape.OnEntityExitColShape += OnEntityExitColShape;
+        * }
+    */
